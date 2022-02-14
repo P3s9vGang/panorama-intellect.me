9e0d90dc2471a700415dc5a59cf4653301c4bc5c
KoT OsKaR <tibeco255@yandex.ru>
2022-02-14 11:43:30 +0300

    image prev in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from .models import Article, OfferedArticle
+from django.utils.safestring import mark_safe
 
 admin.site.site_header = "Администрационная панель"
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('name','date')
+    list_display = ('name','htmlimage','date')
+
+    def htmlimage(self, object):
+        return mark_safe(f'<img src = "{object.image}" style = "height: 50px;">')
 
 class OffArticleAdmin(admin.ModelAdmin):
-    list_display = ('name','date','is_accepted')
+    list_display = ('name','date','htmlimage','is_accepted')
     list_editable = ('is_accepted',)
+
+    def htmlimage(self, object):
+        return mark_safe(f'<img src = "{object.image}" style = "height: 50px;">')
 
 # Register your models here.
 admin.site.register(Article, ArticleAdmin)
